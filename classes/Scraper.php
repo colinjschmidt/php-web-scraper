fdda31ed6178fc8db9ec243ab05682255946ff4c
Colin Schmidt <cjschmidt23@gmail.com>
2012-06-22 17:09:03 -0700

    Added methods to abstract Scraper class with phpunit tests

---
+++ classes/Scraper.php
@@ -0,0 +1,263 @@
+<?php
+/**
+ * This file contains the abstract Scraper class
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * PHP version 5.3
+ * 
+ * @category Scraper
+ * @package  Php-web-scraper
+ * @author   colinschmidt <colinjschmidt@gmail.com>
+ * @license  Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @link     https://github.com/colinschmidt/php-web-scraper
+ * @filesource
+ */
+
+// Set the include path for the project
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../library');
+
+// require simple_html_dom library
+require_once 'simple_html_dom/simple_html_dom.php';
+
+/**
+ * Scraper Class
+ * 
+ * This class contains the base functionality for the web scraper. 
+ * It should be extended for each project.
+ * 
+ * @category  Scraper
+ * @package   Php-web-scraper
+ * @author    colinschmidt <colinjschmidt@gmail.com>
+ * @copyright 2012 Colin Schmidt
+ * @license   Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @version   Release: @package_version@
+ * @link      https://github.com/colinschmidt/php-web-scraper
+ * @abstract
+ */
+abstract class Scraper
+{
+    /**
+     * The folder to write the scraper results to relative to project root
+     *
+     * @var string
+     */
+    protected $resultsFolder = 'results';
+    
+    /**
+     * The Base URL to scrape
+     * 
+     * @var string
+     */
+    protected $url;
+    
+    /**
+     * The HTTP GET param for page number
+     * 
+     * @var string
+     */
+    protected $pageParam = 'page';
+    
+    /**
+     * The HTTP GET param for items per page
+     *
+     * @var string
+     */
+    protected $itemsPerPageParam = 'limit';
+    
+    /**
+     * The page number to scrape
+     * 
+     * @var int
+     */
+    protected $page = 1;
+    
+    /**
+     * The number of items per page
+     * 
+     * @var int
+     */
+    protected $itemsPerPage = 25;
+    
+    /**
+     * getResultsFolder Method
+     *
+     * Returns the path to the folder where scraper results will be stored
+     *
+     * @param string $folder The name of the folder to store the scraped results 
+     * 
+     * @return string The absolute path to the results folder
+     */
+    protected function getResultsFolder($folder)
+    {
+        return __DIR__ . '/../' . $folder;
+    }
+    
+    /**
+     * getFileName Method
+     *
+     * Returns the name of the file where scraper results will be stored
+     *
+     * @return string The name of the file
+     */
+    protected function getFileName()
+    {
+        $fileName = 'scraper-' . date('Y-m-d-h', time()) . '.txt';
+        
+        return $fileName;
+    }
+    
+    /**
+     * getUrl Method
+     * 
+     * Returns the URL to be scraped
+     * 
+     * @return string The url to scrape
+     */
+    protected function getUrl()
+    {
+        $url = $this->url
+             . '?' . $this->pageParam . '=' . $this->page
+             . '&' . $this->itemsPerPageParam . '=' . $this->itemsPerPage;
+        
+        return $url;
+    }
+    
+    /**
+     * getUrl Method
+     *
+     * Returns the simple_html_dom object for the URL
+     * 
+     * @param string $url The URL to be scraped
+     * 
+     * @return Simple_html_dom The simple_html_dom object
+     */
+    protected function getHtml($url)
+    {
+        return file_get_html($url);
+    }
+    
+    /**
+     * getNextPageUrl Method
+     *
+     * Returns the URL of the next page of items to be scraped
+     *
+     * @param string $html The HTML to retrieve the next page URL
+     *
+     * @return string|NULL The next page url to scrape or NULL
+     */
+    abstract protected function getNextPageUrl($html);
+    
+    /**
+     * getItems Method
+     *
+     * Returns an array of simple_html_dom_node objects for each item
+     *
+     * @param string $html The HTML to retrieve the next page URL
+     *
+     * @return array|NULL An array of items or NULL
+     */
+    abstract protected function getItems($html);
+    
+    /**
+     * getItemData Method
+     *
+     * Returns an array of data for an item
+     *
+     * @param Simple_html_dom_node $item simple_html_dom_node object for an item
+     *
+     * @return array|NULL An array of data for an item or NULL
+     */
+    abstract protected function getItemData($item);
+    
+    /**
+     * formatData Method
+     * 
+     * Returns a formatted string of data for an item
+     * 
+     * @param array $data An array of data for an item
+     * 
+     * @return string The formatted string of data to be appended to a file
+     */
+    protected function formatData($data)
+    {
+        return json_encode($data);
+    }
+    
+    /**
+     * appendData Method
+     *
+     * Appends string of data for an item to file
+     *
+     * @param string $file The full path to the file
+     * @param string $str  A string of data for an item
+     *
+     * @return bool True on success, False on failure
+     */
+    protected function appendData($file, $str)
+    {   
+        $handle = fopen($file, 'a');
+        fwrite($handle, $str . "\n");
+        fclose($handle);
+    }
+    
+    /**
+     * Run Method
+     * 
+     * Executes the steps required to scrape a websites content (recursively)
+     * 
+     * @param mixed $url The url of the page to scrape or null
+     * 
+     * @return bool
+     */
+    public function run($url = null)
+    {
+        // If no url was passed in, construct the url from the member variables
+        if (empty($url)) {
+            $url = $this->getUrl();
+        }
+        
+        // Get the simple_html_dom page 
+        $html = $this->getHtml($url);
+        
+        // Get the items from the page html
+        $items = $this->getItems($html);
+        
+        // Create an empty string to start saving data to
+        $str = '';
+        
+        // Loop through all of the items,
+        // formatting the data and appending to the string
+        foreach ($items as $item) {
+            $itemData = $this->getItemData($item);
+            $str .= $this->formatData($itemData);            
+        }
+        
+        // Construct the path to the file to save the results to
+        $file = $this->getResultsFolder($this->resultsFolder) 
+              . '/'
+              . $this->getFileName();
+        
+        // Append formatted data to the file
+        $appended = $this->appendData($file, $str);
+        
+        // Check if there is a next page to scrape
+        $nextPage = $this->getNextPageUrl($html);
+        
+        // If there is a next page and the last page's data was saved,
+        // scrape the next page
+        if ($appended === true && !empty($nextPage)) {
+            $this->run($nextPage);
+        }
+    }
+}
---
+++ tests/classes/ScraperTest.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * This file contains the Unit Tests for the Scraper Class
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * PHP version 5.3
+ *
+ * @category Tests
+ * @package  Php-web-scraper
+ * @author   colinschmidt <colinjschmidt@gmail.com>
+ * @license  Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @link     https://github.com/colinschmidt/php-web-scraper
+ * @filesource
+ */
+
+require_once 'vfsStream/vfsStream.php';
+require_once __DIR__ . '/../../classes/Scraper.php';
+
+/**
+ * Scraper Test Class
+ * 
+ * @category Tests
+ * @package  Php-web-scraper
+ * @author   colinschmidt <colinjschmidt@gmail.com>
+ * @license  Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @link     https://github.com/colinschmidt/php-web-scraper
+ */
+class ScraperTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * An instance of the Scraper Class
+     * 
+     * @var Scraper
+     */
+    protected $scraper;
+    
+    /**
+     * Setup function before each test
+     * 
+     * @return void
+     */
+    public function setup()
+    {   
+        // Get a Mock Object for the abstract Scraper class
+        $scraper = $this->getMockForAbstractClass('Scraper');
+        
+        // Use PHP's reflection API to interact with mock Scraper Class
+        $this->scraper = new ReflectionClass($scraper);
+    }
+    
+    /**
+     * Tests Scraper::getFileName
+     * 
+     * @return void
+     */
+    public function testGetFileName()
+    {
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+        
+        $method = $this->scraper->getMethod('getFileName');
+        $method->setAccessible(true);
+        
+        $this->assertInternalType('string', $method->invoke($mockController));
+    }
+    
+    /**
+     * Tests Scraper::getUrl
+     * 
+     * @return void
+     */
+    public function testGetUrl()
+    {
+        $baseUrl = 'http://google.com/search';
+        
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+        
+        // Define URL property
+        $urlProperty = $this->scraper->getProperty('url');
+        $urlProperty->setAccessible(true);
+        $urlProperty->setValue($mockController, 'http://google.com/search');
+        
+        // Get PageParam property value
+        $pageParamProperty = $this->scraper->getProperty('pageParam');
+        $pageParamProperty->setAccessible(true);
+        $pageParam = $pageParamProperty->getValue($mockController);
+        
+        // Get itemsPerPageParam property value
+        $itemsPerPageParamProperty = $this->scraper
+            ->getProperty('itemsPerPageParam');
+        $itemsPerPageParamProperty->setAccessible(true);
+        $itemsPerPageParam = $itemsPerPageParamProperty
+            ->getValue($mockController);
+        
+        // Get page property value
+        $pageProperty = $this->scraper->getProperty('page');
+        $pageProperty->setAccessible(true);
+        $page = $pageProperty->getValue($mockController);
+        
+        // Get itemsPerPage property value
+        $itemsPerPageProperty = $this->scraper->getProperty('itemsPerPage');
+        $itemsPerPageProperty->setAccessible(true);
+        $itemsPerPage = $itemsPerPageProperty->getValue($mockController);
+        
+        // Generate URL manually to compare with method output
+        $url = "$baseUrl?$pageParam=$page&$itemsPerPageParam=$itemsPerPage";
+        
+        // Get the method and make it accessible
+        $method = $this->scraper->getMethod('getUrl');
+        $method->setAccessible(true);
+        
+        // Assert that the method responses matches the expected output
+        $this->assertEquals($url, $method->invoke($mockController));
+    }
+    
+    /**
+     * Tests Scraper::getHtml
+     * 
+     * @covers Scraper::getHtml
+     * @return void
+     */
+    public function testGetHtml()
+    {
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+        
+        $method = $this->scraper->getMethod('getHtml');
+        $method->setAccessible(true);
+        
+        // Make sure that the response is a simple_html_dom object
+        $this->assertInstanceOf(
+            'Simple_html_dom',
+            $method->invokeArgs($mockController, array('http://google.com'))
+        );
+        
+        // Bad test, not a real url
+        $this->assertFalse(
+            $method->invokeArgs($mockController, array('adfhsdafkjhdsakl'))
+        );
+    }
+    
+    /**
+     * Tests Scraper::formatData
+     * 
+     * @covers Scraper::formatData
+     * @return void
+     */
+    public function testFormatData()
+    {
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+    
+        $method = $this->scraper->getMethod('formatData');
+        $method->setAccessible(true);
+    
+        // Make sure that the response is a simple_html_dom object
+        $this->assertInternalType(
+            'string',
+            $method->invokeArgs($mockController, array(array('test'=>1)))
+        );
+        
+        // Make sure that method is json encoding the data
+        $json = $method->invokeArgs($mockController, array(array('test'=>1)));
+        $this->assertInternalType(
+            'array',
+            json_decode($json, true)
+        );
+        
+        // what if a string is passed in
+        $this->assertInternalType(
+            'string',
+            $method->invokeArgs($mockController, array('test'))
+        );
+        
+        // what if null is passed in
+        $this->assertInternalType(
+            'string',
+            $method->invokeArgs($mockController, array(null))
+        );
+    }
+    
+    /**
+     * Tests Scraper::getResultsFolder
+     *
+     * @covers Scraper::getResultsFolder
+     * @return void
+     */
+    public function testGetResultsFolder()
+    {
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+        
+        $method = $this->scraper->getMethod('getResultsFolder');
+        $method->setAccessible(true);
+        
+        // Successful test, directory exists
+        $directory = $method->invokeArgs(
+            $mockController,
+            array('results')
+        );
+        $this->assertTrue(is_dir($directory));
+        
+        // Non existent directory
+        $directory = $method->invokeArgs(
+            $mockController,
+            array('test')
+        );
+        $this->assertFalse(is_dir($directory));
+    }
+    
+    /**
+     * Tests Scraper::appendData
+     * 
+     * @covers Scraper::appendData
+     * @return void
+     */
+    public function testAppendData()
+    {   
+        // Setup a mock object for Scraper class
+        $mockController = $this->getMockForAbstractClass('Scraper');
+        
+        $method = $this->scraper->getMethod('appendData');
+        $method->setAccessible(true);
+        
+        $property = $this->scraper->getProperty('resultsFolder');
+        $property->setAccessible(true);
+        
+        $fileName = 'test-' . date('Y-m-d-h', time()) . '.txt';
+        
+        $fullPath = __DIR__ . '/../../' 
+            . $property->getValue($mockController)
+            . '/'
+            . $fileName;
+        
+        // Run the appendData method
+        $method->invokeArgs(
+            $mockController,
+            array($fullPath, 'test')
+        );
+        
+        // Assert that the file exists
+        $this->assertTrue(file_exists($fullPath));
+        
+        // Check that the output equals the expected
+        $this->assertEquals("test\n", file_get_contents($fullPath));       
+
+        // Run the appendData method again
+        $method->invokeArgs(
+            $mockController,
+            array($fullPath, 'test2')
+        );
+        
+        // Check that the output equals the expected
+        $this->assertEquals("test\ntest2\n", file_get_contents($fullPath));
+        
+        unlink($fullPath);
+    }
+}
